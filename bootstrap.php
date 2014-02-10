f1b7bfbd08b26478e42a547ea88ee052df670af3
Jirka Koutny <koutny@collabim.com>
2014-02-10 19:40:17 +0100

    Simple autoloader added

--- bootstrap.php
+++ bootstrap.php
@@ -1,22 +1,12 @@
 <?php
 
-require 'library/Collabim/Curl/CurlDownloader/BodyDecoder.php';
-require 'library/Collabim/Curl/CurlDownloader/Config.php';
-require 'library/Collabim/Curl/CurlDownloader/EmptyResponseException.php';
-require 'library/Collabim/Curl/CurlDownloader/IPostData.php';
-require 'library/Collabim/Curl/CurlDownloader/RawPostData.php';
-require 'library/Collabim/Curl/CurlDownloader/Response.php';
-require 'library/Collabim/Curl/CurlDownloader/ResponseParser.php';
-require 'library/Collabim/Curl/CurlDownloader/ResponsesContainer.php';
-require 'library/Collabim/Curl/CurlDownloader.php';
-require 'library/Collabim/FastRpc/BinaryDataEnvelope.php';
-require 'library/Collabim/FastRpc/DataType.php';
-require 'library/Collabim/FastRpc/IRequest.php';
-require 'library/Collabim/FastRpc/RequestEncoder.php';
-require 'library/Collabim/FastRpc/ResponseDecoder.php';
-require 'library/Collabim/FastRpc/SearchRequest.php';
-require 'library/Collabim/Seznam/SerpParser.php';
-require 'library/Collabim/String/Utf8StringHandler.php';
+function __autoload($className) {
+	$libraryFilename = 'library/' . str_replace('\\', '/', $className) . '.php';
+
+	if (is_readable($libraryFilename)) {
+		require $libraryFilename;
+	}
+}
 
 function getSerpParser($searchRpcUrl) {
 	return new \Collabim\Seznam\SerpParser(
